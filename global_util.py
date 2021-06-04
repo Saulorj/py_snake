d25673c8bb4f3038d157bcf29be3b3593de6ce66
Saulo de Oliveira <saccacio@lasa.com.br>
2021-06-03 23:44:41 -0300

    Draw game border

--- global_util.py
+++ global_util.py
@@ -1,4 +1,12 @@
 
 
-width = 640
-height = 480
+width = 1300
+height = 700
+frame_rate = 30
+border = 5
+
+color_white = (255,255,255)
+color_black = (0,0,0)
+color_grey = (166, 165, 162)
+color_green = (0,255,0)
+color_red = (255,0,0)
